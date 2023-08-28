ec94d5d41a32dc106e5fe112e901c912729961bb
Param Pal Singh <singhparampal97@gmail.com>
2023-08-28 21:50:33 +0200

    change nextjs output to export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true;
 
 const nextConfig = {
-  output: process.env.NODE_ENV !== "production" ? undefined : "export",
+  output: "export",
   images: {
-    unoptimized: envImageUnoptimize,
+    unoptimized: true,
   },
 };
 
