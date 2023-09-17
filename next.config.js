8283a2cab5918c034d247607e8a57c5c6734a0af
Param Pal Singh <singhparampal97@gmail.com>
2023-09-17 21:44:23 +0200

    revert to original nextjs config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/space_tourism",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
